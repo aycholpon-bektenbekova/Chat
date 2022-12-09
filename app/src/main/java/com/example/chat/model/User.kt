48091cd21b1ad78625c49a23a89f28d9f8f464be
DELL <aycholpon25t@gmail.com>
2022-12-09 19:16:08 +0600

    added ChatsAdapter

--- app/src/main/java/com/example/chat/model/User.kt
+++ app/src/main/java/com/example/chat/model/User.kt
@@ -1,7 +1,9 @@
 package com.example.chat.model
 
+import java.io.Serializable
+
 data class User(
-    var userName: String,
-    var phone: String,
+    var userName: String? = null,
+    var phone: String? = null,
     var uid: String? = null,
-)
+) : Serializable
